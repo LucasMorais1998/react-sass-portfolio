c78aa898bbb561bc4c2fa1ca312f7b7ad3cd7ea9
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-02 20:37:53 -0300

    import components into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import MainContent from "./components/MainContent";
+import Sidebar from "./components/Sidebar";
+
 import "./styles/components/app.scss";
 
 function App() {
   return (
     <div id="portfolio">
       <h1>Lorem Ipsum.</h1>
+      <Sidebar />
+      <MainContent />
     </div>
   );
 }
