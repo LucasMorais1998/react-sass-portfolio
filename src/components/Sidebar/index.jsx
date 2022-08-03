994b642ce20fe4dfbc6ac68f19412d3597241af4
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-02 21:12:25 -0300

    config Sidebar component

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,8 +1,16 @@
+import AvatarImg from "../../assets/images/person-picture.jpg";
+
 const Sidebar = () => {
   return (
-    <div>
-      <h1>Sidebar</h1>
-    </div>
+    <aside id="sidebar">
+      <p>imagem</p>
+      <p className="title">Desenvolvedor</p>
+      <p>redes sociais</p>
+      <p>informações de contato</p>
+      <a href="" className="btn">
+        Download currículo
+      </a>
+    </aside>
   );
 };
 
