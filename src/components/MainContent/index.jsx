d0050574d47cd1b53a3357cbd048706e4c45b7b6
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-02 22:27:12 -0300

    import ProjectsContainer in MainContent

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -1,6 +1,7 @@
 /* import "../../styles/components/maincontent.scss"; */
 
 import AboutContainer from "../AboutContainer";
+import ProjectsContainer from "../ProjectsContainer";
 import TechnologiesContainer from "../TechnologiesContainer";
 
 const MainContent = () => {
@@ -8,6 +9,7 @@ const MainContent = () => {
     <main id="main-content">
       <AboutContainer />
       <TechnologiesContainer />
+      <ProjectsContainer />
     </main>
   );
 };
