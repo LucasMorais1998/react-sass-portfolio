1b77f089c5f87511906faa8c0a0513b51af468da
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-02 22:26:08 -0300

    add ProjectsContainer component

---
+++ src/components/ProjectsContainer/index.jsx
@@ -0,0 +1,18 @@
+const ProjectsContainer = () => {
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae
+        excepturi sit officiis molestiae, cupiditate asperiores rerum.
+        Voluptatibus esse maxime fugit fugiat maiores. Ullam, nesciunt tenetur
+        libero autem ex quia! Minima.
+      </p>
+      <a href="#" className="btn">
+        Ver Projetos
+      </a>
+    </section>
+  );
+};
+
+export default ProjectsContainer;
